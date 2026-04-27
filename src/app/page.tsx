66df155992225392447d7efd2ebd0ec2c22fdc0a
Nyame100 <nyamerichmond100@gmail.com>
2026-04-27 22:19:21 +0200

    feat: add interactive Sectors section to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Insights from "@/components/home/Insights";
 import Marquee from "@/components/home/Marquee";
 import ProcessSteps from "@/components/home/ProcessSteps";
 import ProjectsGrid from "@/components/home/ProjectsGrid";
+import Sectors from "@/components/home/Sectors";
 import ServicesGrid from "@/components/home/ServicesGrid";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <Marquee/>
       <ServicesGrid/>
       <ProjectsGrid/>
+      <Sectors/>
       <ProcessSteps/>
       <Insights/>
       <CTABand/>
